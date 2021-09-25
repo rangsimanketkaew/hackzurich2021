32afa073e67b4d7d1b95f0f192b4895c716e134d
Rangsiman Ketkaew <rangsiman.ketkaew@chem.uzh.ch>
2021-09-25 03:42:21 +0200

    update

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+
+import sys
+sys.setrecursionlimit(1500)
+
 import os
 import tkinter as tk
 import tkinter.scrolledtext as tkscrolled
@@ -9,7 +14,7 @@ from tkinter.messagebox import showinfo
 
 from datetime import datetime
 import webbrowser
-# from urllib.request import urlopen
+
 # from PIL import ImageTk, Image
 
 ## For barcode scanner
@@ -17,24 +22,37 @@ import cv2
 import numpy as np
 from pyzbar.pyzbar import decode
 
-def loadrelimages(relativepath):
-    from PIL import ImageTk, Image
-    import os
-    directory_path = os.path.dirname(__file__)
-    file_path = os.path.join(directory_path, relativepath)
-    img = ImageTk.PhotoImage(Image.open(file_path.replace('\\',"/")))  
-    return img
+## JSON API
+import urllib.request, json 
+
 
 class Mygarden:
     """Mygarden main app
     """
     def __init__(self):
-        self.master = tk.Tk()
+        self.barcodes = set([])
+        self.total_co2 = 0
+        self.score = 0
+        self.num_trees = 0
 
+        self.master = tk.Tk()
         self.start_master()
         self.add_menu()
         self.add_widgets()
+        self.welcome_msg()
         self.start_app()
+
+    ##########
+    # Helper #
+    ##########
+    def loadrelimages(self, relativepath):
+        from PIL import ImageTk, Image
+        directory_path = os.path.dirname(__file__)
+        file_path = os.path.join(directory_path, relativepath)
+        image = Image.open(file_path.replace('\\',"/"))
+        image = image.resize((200, 200), Image.ANTIALIAS)
+        img = ImageTk.PhotoImage(image)  
+        return img
 
     def start_master(self):
         """Start application with UI settings
@@ -77,46 +95,181 @@ class Mygarden:
         # Frame 1 #
         ###########
 
-        frame1 = tk.LabelFrame(self.master, text="Program Console")
+        frame1 = tk.LabelFrame(self.master, text="")
         frame1.grid(padx=5, pady=5, ipadx=2, ipady=2, sticky=tk.N, row=0, column=0)
 
-        btn1 = ttk.Button(frame1, text="Inventory", command=self.test_command)
-        btn1.config(width=14)
-        btn1.grid(padx="10", pady="5", row=0, column=0)
-        btn2 = ttk.Button(frame1, text="Scan", command=self.test_command)
-        btn2.config(width=14)
-        btn2.grid(padx="10", pady="5", row=0, column=1)
+        self.btn1 = ttk.Button(frame1, text="Inventory", command=self.test_command)
+        self.btn1.config(width=14)
+        self.btn1.grid(padx="10", pady="5", row=0, column=0)
+        #---------
+        self.btn2 = ttk.Button(frame1, text="Scan", command=self.scanner)
+        self.btn2.config(width=14)
+        self.btn2.grid(padx="10", pady="5", row=0, column=1)
+        #---------
+        self.btn3 = ttk.Button(frame1, text="Print codes", command=self.print_codes)
+        self.btn3.config(width=14)
+        self.btn3.grid(padx="10", pady="5", row=0, column=2)
+        #---------
+        self.btn3 = ttk.Button(frame1, text="Calculate score", command=self.calc_score)
+        self.btn3.config(width=14)
+        self.btn3.grid(padx="10", pady="5", row=0, column=3)
 
         ###########
         # Frame 2 #
         ###########
 
-        frame2 = tk.Frame(self.master)
+        frame2 = tk.LabelFrame(self.master, text="Progress")
         frame2.grid(padx=5, pady=10, row=1, column=0, columnspan=3)
-        self.box_result = tkscrolled.ScrolledText(frame2)
-        self.box_result.configure(height="19", width="70", wrap="word", undo="True")
-        self.box_result.grid(row=0)
 
-        # # show image
-        # def show_tree(self):
-        #     canvas = tk.Canvas(self.master, width=300, height=300)
-        #     canvas.pack()
-        #     img = PhotoImage
+        self.box_product = tkscrolled.ScrolledText(frame2)
+        self.box_product.configure(height="10", width="40", wrap="word", undo="True")
+        self.box_product.grid(row=0, column=0)
+
+        self.box_score = tkscrolled.ScrolledText(frame2)
+        self.box_score.configure(height="10", width="40", wrap="word", undo="True")
+        self.box_score.grid(row=0, column=1)
 
         ###########
         # Frame 3 #
         ###########
+
         frame3 = tk.Frame(self.master)
         frame3.grid(column=0, columnspan=3)
-        # img = ImageTk.PhotoImage(Image.open("assets/apple.jpg"))
-        canvas = tk.Canvas(frame3, width=200, height=200)
-        loadedimage = loadrelimages('assets/oak.jpg')
-        canvas.create_image(200, 200, image=loadedimage)
-        # canvas.create_image(200, 200, image=img)
-        canvas.pack()
 
-    # def scanner(self):
+        self.canvas_1 = tk.Canvas(frame3, width=100, height=100)
+        self.loadedimage_1 = self.loadrelimages('assets/apple.jpg')
+        self.canvas_1.create_image(50, 50, anchor="center", image=self.loadedimage_1)
+        self.canvas_1.grid(row=0, column=0)
 
+        self.canvas_2 = tk.Canvas(frame3, width=100, height=100)
+        self.loadedimage_2 = self.loadrelimages('assets/beehive.jpg')
+        self.canvas_2.create_image(50, 50, anchor="center", image=self.loadedimage_2)
+        self.canvas_2.grid(row=0, column=1)
+
+        self.canvas_3 = tk.Canvas(frame3, width=100, height=100)
+        self.loadedimage_3 = self.loadrelimages('assets/chickens.jpg')
+        self.canvas_3.create_image(50, 50, anchor="center", image=self.loadedimage_3)
+        self.canvas_3.grid(row=0, column=2)
+
+        self.canvas_4 = tk.Canvas(frame3, width=100, height=100)
+        self.loadedimage_4 = self.loadrelimages('assets/cows.jpg')
+        self.canvas_4.create_image(50, 50, anchor="center", image=self.loadedimage_4)
+        self.canvas_4.grid(row=0, column=3)
+
+        self.canvas_5 = tk.Canvas(frame3, width=100, height=100)
+        self.loadedimage_5 = self.loadrelimages('assets/pigs.jpg')
+        self.canvas_5.create_image(50, 50, anchor="center", image=self.loadedimage_5)
+        self.canvas_5.grid(row=0, column=4)
+
+    ###################
+    # Barcode scanner #
+    ###################
+
+    def decoder(self, image):
+        gray_img = cv2.cvtColor(image,0)
+        barcode = decode(gray_img)
+        barcodeData = 0
+        
+        for obj in barcode:
+            points = obj.polygon
+            (x,y,w,h) = obj.rect
+            pts = np.array(points, np.int32)
+            pts = pts.reshape((-1, 1, 2))
+            cv2.polylines(image, [pts], True, (0, 255, 0), 3)
+
+            barcodeData = obj.data.decode("utf-8")
+            barcodeType = obj.type
+            string = "Data " + str(barcodeData) + " | Type " + str(barcodeType)
+            
+            cv2.putText(image, string, (x,y), cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,0,0), 2)
+            print("Barcode: " + barcodeData +" | Type: "+barcodeType)
+
+            return int(barcodeData)
+
+    def scanner(self):
+        cap = cv2.VideoCapture(0)
+        no_product = 1
+        while True:
+            ret, frame = cap.read()
+            barcode_data = self.decoder(frame)
+            # store unique barcode
+            if barcode_data not in self.barcodes:
+                # self.show_text(barcode_data)
+                print("yes")
+                self.show_text_product(f"Product {no_product} added")
+                no_product += 1
+
+            self.barcodes.add(barcode_data)
+
+            ## shutdown scanner when detected
+            # if int(barcode_data) > 1:
+            #     cv2.destroyWindow(winname="Close")
+            #     break
+
+            cv2.imshow('Scan your product', frame)
+            code = cv2.waitKey(10)
+            # press "q" to close the scanner
+            if code == ord('q'):
+                break
+
+        # cap.release()
+        cv2.destroyAllWindows()
+
+    # def show_barcode(self):
+    #     self.show_text_product(print(self.barcodes))
+
+    def print_codes(self):
+        for barcode in self.barcodes:
+            self.show_text_product("===> " + str(barcode))
+
+    ###################
+    # Calculate score #
+    ###################
+
+    def calc_score(self):
+        from pprint import pprint
+        headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
+        chaper_url = "https://tools.learningcontainer.com/sample-json-file.json"
+        req = urllib.request.Request(url=chaper_url, headers=headers)
+        data = urllib.request.urlopen(req).read()
+        data = json.loads(data.decode())
+        print(data)
+
+        self.show_text_score(str(data))
+
+    ##########################
+    # Text printer (frame 3) #
+    ##########################
+
+    def show_text_product(self, text):
+        """Insert text to result box
+        """
+        self.box_product.insert(tk.INSERT, text + "\n")
+        self.box_product.see(tk.END)
+
+    def clear_box_product(self):
+        """Clear box
+        """
+        self.box_product.delete(1.0, tk.END)
+
+    def show_text_score(self, text):
+        """Insert text to result box
+        """
+        self.box_score.insert(tk.INSERT, text + "\n")
+        self.box_score.see(tk.END)
+
+    def clear_box_score(self):
+        """Clear box
+        """
+        self.box_score.delete(1.0, tk.END)
+
+    ####################################################
+
+    def welcome_msg(self):
+        """Show welcome message in result box:
+        """
+        self.show_text_product(f"Welcome to MyGarden version 0.1\n")
+        self.show_text_product(f"Developed in HackZurich 2021\n")
 
     def start_app(self):
         """Start application
@@ -126,19 +279,7 @@ class Mygarden:
     ###### All functions should be defined from here on ######
     def test_command(self):
         print("ok")
-    
-# window.title("Welcome to [M]yGarden")
-# window.geometry('500x300')
 
-# btt = tk.Button(window, text="Enter", command = helloCallBack())
-
-# chk_state = tk.BooleanVar()
-# chk_state.set(True) #set check state
-# chk = tk.Checkbutton(window, text='Choose', var=chk_state)
-# chk.grid(column=0, row=0)
-# btt.grid(column=0, row=1)
-
-# window.mainloop()
 
 def main():
     app = Mygarden()
--- test.py
+++ test.py
@@ -1,30 +1,11 @@
-# from tkinter import *
-# root = Tk()
-
-# canv = Canvas(root, width=80, height=80, bg='white')
-# canv.grid(row=2, column=3)
-
-# img = PhotoImage(file="assets/apple.jpg")
-# canv.create_image(20,20, anchor=NW, image=img)
-
-# mainloop()
-
-from tkinter import *
+import tkinter as tk
 import os
 
 from PIL import ImageTk, Image
 
-# def loadrelimages(relativepath):
-#     from PIL import ImageTk, Image
-#     import os
-#     directory_path = os.path.dirname(__file__)
-#     file_path = os.path.join(directory_path, relativepath)
-#     img = ImageTk.PhotoImage(Image.open(file_path.replace('\\',"/")))  
-#     return img
+root = tk.Tk()
 
-root = Tk()
-
-canvas = Canvas(root, width=500, height=500)
+canvas = tk.Canvas(root, width=500, height=500)
 img = ImageTk.PhotoImage(Image.open("assets/apple.jpg"))
 
 # loadedimage=loadrelimages('assets/apple.jpg')
